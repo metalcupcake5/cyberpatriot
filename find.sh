0bb81992cbff2d4fdd83f975831d5bf020297e98
metalcupcake5 <imcoolcom5@gmail.com>
2021-11-12 00:06:13 -0800

    change formatting and add more files to find

--- find.sh
+++ find.sh
@@ -1,1 +1,24 @@
-(find $(pwd) -name "*.txt"; find $(pwd) -name "*.png"; find $(pwd) -name "*.mp4"; find $(pwd) -name "*.zip"; find $(pwd) -name "*.rar") | tee find.txt
+(
+    ## pictures
+    find / -name "*.png" -type f;
+    find / -name "*.jpg" -type f;
+    find / -name "*.jpeg" -type f;
+    find / -name "*.gif" -type f;
+
+    ## video
+    find / -name "*.mp4" -type f;
+    find / -name "*.mov" -type f;
+
+    ## audio
+    find / -name "*.mp3" -type f;
+    find / -name "*.wav" -type f;
+    
+    ## others
+    find / -name "*.txt" -type f;
+    find / -name "*.zip" -type f;
+    find / -name "*.rar" -type f;
+    find / -name "*.tar.gz" -type f;
+    find / -name "*.php" -type f;
+    find / -name "*backdoor*.*" -type f;
+    find / -name "*backdoor*.php" -type f;
+) | tee find.txt
