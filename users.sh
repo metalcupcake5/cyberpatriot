e3d4ea705630413651b4f32c4248e36c23ca057e
metalcupcake5 <imcoolcom5@gmail.com>
2021-11-12 00:48:13 -0800

    update users.sh

--- users.sh
+++ users.sh
@@ -2,6 +2,8 @@ echo "users script"
 echo "1. list users"
 echo "2. list groups"
 echo "3. view user groups"
+echo "4. remove user admin"
+echo "5. add user admin"
 
 read choice
 
@@ -16,6 +18,22 @@ then
     echo "what user"
     read user
     groups $user
+elif [ $choice == 4 ]
+then
+    echo "what user"
+    read user
+    gpasswd -d $user sudo
+    gpasswd -d $user adm
+    gpasswd -d $user lpadmin
+    gpasswd -d $user sambashare
+elif [ $choice == 5 ]
+then
+    echo "what user"
+    read user
+    gpasswd -a $user sudo
+    gpasswd -a $user adm
+    gpasswd -a $user lpadmin
+    gpasswd -a $user sambashare
 else
     echo "you didn't choose anyting"
 fi
