13da63a625b723149ce1d75973b2b2c8308b139c
metalcupcake5 <imcoolcom5@gmail.com>
2022-12-09 01:42:59 -0800

    feat: find in current dir, dont search configs

--- find.sh
+++ find.sh
@@ -1,24 +1,24 @@
 (
     ## pictures
-    find / -name "*.png" -type f;
-    find / -name "*.jpg" -type f;
-    find / -name "*.jpeg" -type f;
-    find / -name "*.gif" -type f;
+    find ./ -name "*.png" -type f;
+    find ./ -name "*.jpg" -type f;
+    find ./ -name "*.jpeg" -type f;
+    find ./ -name "*.gif" -type f;
 
     ## video
-    find / -name "*.mp4" -type f;
-    find / -name "*.mov" -type f;
-    find / -name "*.webm" -type f;
+    find ./ -name "*.mp4" -type f;
+    find ./ -name "*.mov" -type f;
+    find ./ -name "*.webm" -type f;
 
     ## audio
-    find / -name "*.mp3" -type f;
-    find / -name "*.wav" -type f;
-    
+    find ./ -name "*.mp3" -type f;
+    find ./ -name "*.wav" -type f;
+
     ## others
-    find / -name "*.txt" -type f;
-    find / -name "*.zip" -type f;
-    find / -name "*.rar" -type f;
-    find / -name "*.tar.gz" -type f;
-    find / -name "*.php" -type f;
-    find / -name "*backdoor*.*" -type f;
-) | tee find.txt
+    find ./ -name "*.txt" -type f;
+    find ./ -name "*.zip" -type f;
+    find ./ -name "*.rar" -type f;
+    find ./ -name "*.tar.gz" -type f;
+    find ./ -name "*.php" -type f;
+    find ./ -name "*backdoor*.*" -type f;
+) | tee find.txt | grep -v "/\."
